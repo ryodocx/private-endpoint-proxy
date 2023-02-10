2b8a245c8b315105f1285a7fc22e2eff151e287c
ryodocx <email@ryodocx.net>
2023-02-10 22:50:07 +0900

    healthcheck endpoint

---
+++ pkg/server/endpoint_healthcheck.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (s server) live(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "live")
+
+}
+func (s server) ready(w http.ResponseWriter, r *http.Request) {
+	// TODO
+	fmt.Fprintln(w, "ready")
+}
